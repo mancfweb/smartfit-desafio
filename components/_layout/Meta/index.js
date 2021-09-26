dd6b6af00696e73647973851d0ee86e287cb057c
Marcus Cavalcanti <marcus.cavalcanti20@gmail.com>
2021-09-26 20:09:00 -0300

    updated meta tags and favicon

--- components/_layout/Meta/index.js
+++ components/_layout/Meta/index.js
@@ -19,19 +19,10 @@ const Meta = () => (
       sizes="16x16"
       href="/favicon/favicon-16x16.png"
     />
-    <link rel="manifest" href="/favicon/site.webmanifest" />
-    <link
-      rel="mask-icon"
-      href="/favicon/safari-pinned-tab.svg"
-      color="#000000"
-    />
-    <link rel="shortcut icon" href="/favicon/favicon.ico" />
-    <meta name="msapplication-TileColor" content="#000000" />
-    <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
-    <meta name="theme-color" content="#000" />
-    <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
+    <meta name="msapplication-TileColor" content="#ffffff" />
+    <meta name="theme-color" content="#ffffff" />
     <meta name="description" content="Smart Fit - Unidades" />
-    <meta property="og:image" content="url_image_para_share" />
+    <title>Smart Fit - Encontre sua unidade</title>
   </Head>
 )
 
